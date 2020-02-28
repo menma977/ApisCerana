0c4b7e474715b2f2a9ef51ecabe6f4c824b87ef0
Venom <com.owl.minerva@gmail.com>
2020-02-28 17:29:03 +0700

    28/02/2020 #1 Fix Image Bug

--- app/src/main/java/com/apis/cerana/controller/ImageGeneratorController.kt
+++ app/src/main/java/com/apis/cerana/controller/ImageGeneratorController.kt
@@ -3,23 +3,22 @@ package com.apis.cerana.controller
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
-import java.lang.Exception
 import java.net.URL
 
-class ImageGeneratorController(private var url: String) : AsyncTask<Void, Void, Bitmap>() {
+class ImageGeneratorController(private val url: String) : AsyncTask<Void, Void, Bitmap>() {
   override fun doInBackground(vararg params: Void?): Bitmap {
     return try {
-      val urlImage: URL = if (url.isEmpty()) {
-        URL("https://malahabel.com/dist/img/ApisMelli.png")
+      return if (url.isEmpty() || url == "null") {
+        val urlImage = URL("https://malahabel.com/dist/img/ApisMelli.png")
+        BitmapFactory.decodeStream(urlImage.openConnection().getInputStream())
       } else {
-        URL(url)
+        val urlImage = URL(url)
+        BitmapFactory.decodeStream(urlImage.openConnection().getInputStream())
       }
-      BitmapFactory.decodeStream(urlImage.openConnection().getInputStream())
     } catch (e: Exception) {
       e.printStackTrace()
-      val url =
-        URL("https://malahabel.com/dist/img/ApisMelli.png")
-      BitmapFactory.decodeStream(url.openConnection().getInputStream())
+      val urlImage = URL("https://malahabel.com/dist/img/ApisMelli.png")
+      BitmapFactory.decodeStream(urlImage.openConnection().getInputStream())
     }
   }
 }
